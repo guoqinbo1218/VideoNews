619d9dc8031048c17c3089fd46cba1bb39b4480f
yuanchao <yuanchao@feicuiedu.com>
2016-08-19 15:51:26 +0800

    完成列表视图上的视频播放

--- app/src/main/java/com/feicuiedu/videonews/ui/news/NewsItemView.java
+++ app/src/main/java/com/feicuiedu/videonews/ui/news/NewsItemView.java
@@ -1,7 +1,9 @@
 package com.feicuiedu.videonews.ui.news;
 
 import android.content.Context;
+import android.graphics.SurfaceTexture;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,6 +15,7 @@ import com.feicuiedu.videonews.bombapi.entity.NewsEntity;
 import com.feicuiedu.videonews.commons.CommonUtils;
 import com.feicuiedu.videonews.ui.base.BaseItemView;
 import com.feicuiedu.videonews.ui.comments.CommentsActivity;
+import com.feicuiedu.videoplayer.list.MediaPlayerManager;
 import com.squareup.picasso.Picasso;
 
 import butterknife.BindView;
@@ -21,11 +24,12 @@ import butterknife.OnClick;
 
 /**
  * 新闻列表的单项视图, 将使用 MediaPlayer播放视频,TextureView来显示视频
- *
+ * <p/>
  * 作者：yuanchao on 2016/8/17 0017 14:51
  * 邮箱：yuanchao@feicuiedu.com
  */
-public class NewsItemView extends BaseItemView<NewsEntity> {
+public class NewsItemView extends BaseItemView<NewsEntity>
+        implements TextureView.SurfaceTextureListener, MediaPlayerManager.OnPlaybackListener {
     @BindView(R.id.textureView) TextureView textureView;
     @BindView(R.id.ivPreview) ImageView ivPreview;
     @BindView(R.id.tvNewsTitle) TextView tvNewsTitle;
@@ -34,6 +38,8 @@ public class NewsItemView extends BaseItemView<NewsEntity> {
     @BindView(R.id.ivPlay) ImageView ivPlay;
 
     private NewsEntity newsEntity;
+    private MediaPlayerManager mediaPlayerManager;
+    private Surface surface;
 
     public NewsItemView(Context context) {
         super(context);
@@ -42,6 +48,11 @@ public class NewsItemView extends BaseItemView<NewsEntity> {
     @Override protected void initView() {
         LayoutInflater.from(getContext()).inflate(R.layout.item_news, this, true);
         ButterKnife.bind(this);
+
+        mediaPlayerManager = MediaPlayerManager.getsInstance(getContext());
+        mediaPlayerManager.addPlaybackListener(this);
+        //
+        textureView.setSurfaceTextureListener(this);
     }
 
     @Override protected void bindModel(NewsEntity newsEntity) {
@@ -61,7 +72,89 @@ public class NewsItemView extends BaseItemView<NewsEntity> {
     }
 
     @OnClick(R.id.tvCreatedAt)
-    public void navigateToComments(){
+    public void navigateToComments() {
         CommentsActivity.open(getContext(), newsEntity);
+    }
+
+    @OnClick(R.id.ivPreview)
+    public void startPlay() {
+        if (surface == null) {
+            return;
+        }
+        String path = newsEntity.getVideoUrl();
+        String videoId = newsEntity.getObjectId();
+        mediaPlayerManager.startPlayer(surface, path, videoId);
+    }
+
+    @OnClick(R.id.textureView)
+    public void stopPlay() {
+        mediaPlayerManager.stopPlayer();
+    }
+
+    // --------------------------------------------------------
+    @Override public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Surface s = new Surface(surface);
+        this.surface = s;
+    }
+
+    @Override public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        this.surface.release();
+        this.surface = null;
+        // 谁离开，停止谁
+        if (newsEntity.getObjectId().equals(mediaPlayerManager.getVideoId())) {
+            mediaPlayerManager.stopPlayer();
+        }
+        return false;
+    }
+
+    @Override public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+
+    // --------------------------------------------------------
+    private boolean isCurrentVideo(String videoId) {
+        if (videoId == null || newsEntity == null) return false;
+        return newsEntity.getObjectId().equals(videoId);
+    }
+
+    @Override public void onStartBuffering(String videoId) {
+        if (isCurrentVideo(videoId)) {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override public void onStopBuffering(String videoId) {
+        if (isCurrentVideo(videoId)) {
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override public void onStartPlay(String videoId) {
+        if (isCurrentVideo(videoId)) {
+            // 显示TextureView，隐藏预览图
+            ivPreview.setVisibility(View.INVISIBLE);
+            tvNewsTitle.setVisibility(View.INVISIBLE);
+            ivPlay.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override public void onStopPlay(String videoId) {
+        if (isCurrentVideo(videoId)) {
+            ivPreview.setVisibility(View.VISIBLE);
+            tvNewsTitle.setVisibility(View.VISIBLE);
+            ivPlay.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override public void onSizeMeasured(String videoId, int width, int height) {
+        if (isCurrentVideo(videoId)) {
+            // 调整大小
+        }
     }
 }
